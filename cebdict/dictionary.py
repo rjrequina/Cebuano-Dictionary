475d11f5574f4cc1abfdbde4814e725452bace40
Arjemariel Requina <rjrequina@gmail.com>
2018-04-21 06:19:42 +0800

    Functions for dictionary

---
+++ cebdict/dictionary.py
@@ -0,0 +1,51 @@
+import string
+from utilities import read_file
+
+
+'''
+Get the dictionary entries
+'''
+def get_entries():
+	entries = read_file(name='data/cebposdict.txt', strip=True, dict_format=True)
+
+	func_words = ['CONJ', 'DET', 'PART', 'PRON']
+
+	for func in func_words:
+		words = read_file(name='data/function_words/' + func + '.txt', strip=True)
+
+		for word in words:
+			if word in entries:
+				entries[word].append(func)
+			else:
+				entries[word] = [func]
+
+	return entries
+
+
+
+dictionary = get_entries()
+
+
+'''
+Search term in dictionary
+'''
+def search(term=''):
+	term = term.lower()
+	if term not in dictionary:
+		term = string.replace(term, 'o', 'u')
+		term = string.replace(term, 'e', 'i')
+
+	if term not in dictionary:
+		return None
+
+	return dictionary[term]
+
+
+'''
+Checks if the term exists in the dictionary
+'''
+def is_entry(term=''):
+	term = term.lower()
+	term = string.replace(term, 'o', 'u')
+	term = string.replace(term, 'e', 'i')
+	return term in dictionary
