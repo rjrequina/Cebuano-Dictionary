df88482b53637475a7aeae09e45cfee172502f7c
RL-007 <rl-007@RL-007s-iMac.local>
2018-04-24 19:16:34 +0800

    Merged to one file

--- cebdict/dictionary.py
+++ cebdict/dictionary.py
@@ -1,5 +1,66 @@
 import string
-from utilities import read_file
+
+'''
+Write contents to file
+Default is per_line and mode is 'w'
+'''
+def write_file(name=None, contents=[], per_line=True, mode="w", add_newline=True, no_encode=False, append_newline=False):
+    if name:
+        
+        name = sys.prefix + '/' + name
+
+        f = open(name, mode)
+        string = ""
+        for content in contents:
+            if append_newline:
+                content = content + '\n'
+            if per_line:
+                if no_encode:
+                    f.write(content)
+                else:
+                    f.write(content.encode('utf-8'))
+                if add_newline:
+                    f.write("\n")
+            else:
+                string = string + content
+
+        if not per_line:
+            f.write(string)
+
+        f.close()
+    
+    return None
+
+'''
+Returns contents of a file
+Can specify start and end of contents in reading a file
+'''
+def read_file(name=None, start=None, end=None, strip=False, dict_format=False, decode=False):
+    if name:
+
+        name = sys.prefix + '/' + name
+        
+        f = open(name, "r")
+        contents = []
+        dictionary = {}
+        for line in f:
+            if strip:
+                line = line.strip()
+            splitted = line.split(" ")
+            dictionary[splitted[0]] = splitted[1:]
+            if decode:
+                line = line.decode('utf-8')
+            contents.append(line)
+
+        f.close()
+        if start is not None:
+            contents = contents[start:]
+
+        if dict_format:
+            return dictionary
+
+        return contents
+    return None
 
 
 '''
@@ -46,6 +107,6 @@ Checks if the term exists in the dictionary
 '''
 def is_entry(term=''):
 	term = term.lower()
-	term = term.replace(term, 'o', 'u')
-	term = term.replace(term, 'e', 'i')
+	term = term.replace('o', 'u')
+	term = term.replace('e', 'i')
 	return term in dictionary
